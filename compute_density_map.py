f65e4c7eaecb80ac5440ba9056a7c2a979b18cdc
Ben Wibking <wibking.1@astronomy.ohio-state.edu>
2018-01-03 15:59:17 -0500

    updated defaults; added examples

--- compute_density_map.py
+++ compute_density_map.py
@@ -5,21 +5,24 @@ import pynbody
 import pyximport; pyximport.install()
 import cic_density as density
 
-def compute_FFT_densities(particles_filename, output_filename, ngrid=2048):
+def compute_FFT_densities(particles_filename, output_filename,
+                          ngrid=1000, rescale=1.0, do_tanh_stretch=True,
+                          interpolation='lanczos',
+                          pixel_size=2560):
     # read particles
     particles_file = pynbody.load(particles_filename)
     particles = particles_file['pos']
 
     # determine boxsize
-    particles_x = particles['x'][mask]
+    particles_x = particles[:,0]
     boxsize = np.abs(np.max(particles_x) - np.min(particles_x))
 
     # determine the line-of-sight depth of the 2d projection
-    zmin = 0.
-    zmax = 0.8 * boxsize
-    mask = np.logical_and(particles['z'] < zmax, particles['z'] > zmin)
-    particles_x = particles['x'][mask]
-    particles_y = particles['y'][mask]
+    zmin = 0.8 * boxsize
+    zmax = 1.0 * boxsize
+    mask = np.logical_and(particles[:,2] < zmax, particles[:,2] > zmin)
+    particles_x = particles[:,0][mask]
+    particles_y = particles[:,1][mask]
 
     # determine rectangle in x-y plane to plot
     xmin = 0.
@@ -39,40 +42,46 @@ def compute_FFT_densities(particles_filename, output_filename, ngrid=2048):
     import matplotlib.pyplot as plt
     import matplotlib.cm as cm
     from mpl_toolkits.axes_grid1 import make_axes_locatable
-    plt,ax = plt.subplots(1,1)
+    fig,ax = plt.subplots(1,1)
+    dpi = float(fig.get_dpi())
+    pixels = float(pixel_size)
+    fig.set_size_inches([pixels/dpi,pixels/dpi])
+    a=fig.gca()
+    a.set_frame_on(False)
+    a.set_xticks([])
+    a.set_yticks([])
+    plt.axis('off')
 
     # compute 2d density field
     rhogrid = np.empty((ngrid,ngrid),dtype=np.float32)
     rhogrid = np.array(density.cic_2d_grid(particles_x,particles_y,rhogrid,ngrid,boxsize))
-    rhogrid = np.tanh(rhogrid) # apply density stretch so it looks nice
+    rhogrid *= rescale
+    if do_tanh_stretch == True: # apply density stretch so it looks nice
+        rhogrid = np.tanh(rhogrid)
 
     # plot 2d density field
-    image = ax.imshow(rhogrid, cmap=cm.get_cmap('magma'),
-                      extent=[xmin,xmax,ymin,ymax],interpolation='bilinear')
+    image = ax.imshow(rhogrid,
+                      cmap=cm.get_cmap('magma'),
+                      extent=[xmin,xmax,ymin,ymax],
+                      interpolation=interpolation)
 
-    # add colorbar
-    divider = make_axes_locatable(ax)
-    cax = divider.append_axes("right", size="5%", pad=0.1)
-    plt.colorbar(image,cax=cax)
-    
-    # set axes properties
-    ax.set_xlim(xmin,xmax)
-    ax.set_ylim(ymin,ymax)
-    ax.set_xlabel(r'x ($h^{-1}$ Mpc)')
-    ax.set_ylabel(r'y ($h^{-1}$ Mpc)')
-
-    # remove whitespace around edges
-    plt.tight_layout()
-
-    # save in desired file format
-    plt.savefig(output_filename)
+    # save as the given filename in desired file format (based on suffix)
+    fig.savefig(output_filename, bbox_inches='tight', pad_inches=0, dpi='figure')
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('input_particles_filename')
     parser.add_argument('output_density_filename')
+    parser.add_argument('--tanh-stretch',default=True,action='store_true')
+    parser.add_argument('--rescale',type=float,default=0.15)
+    parser.add_argument('--ngrid',type=int,default=1000)
+    parser.add_argument('--interpolation',default='lanczos')
     args = parser.parse_args()
 
     compute_FFT_densities(args.input_particles_filename,
-                          args.output_density_filename)
+                          args.output_density_filename,
+                          ngrid=args.ngrid,
+                          rescale=args.rescale,
+                          do_tanh_stretch=args.tanh_stretch,
+                          interpolation=args.interpolation)
